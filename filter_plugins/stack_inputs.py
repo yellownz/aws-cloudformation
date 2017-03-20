329d85f7592a2fa58973c6144b9b47120a036670
Justin Menga <justin.menga@gmail.com>
2017-03-21 09:48:27 +1300

    Change function naming and variables to snake_case to avoid confusion

--- filter_plugins/stack_inputs.py
+++ filter_plugins/stack_inputs.py
@@ -7,7 +7,7 @@ class FilterModule(object):
         'stack_inputs': stack_inputs
     }
 
-def camel_case(text):
+def snake_case(text):
   str1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', text)
   return re.sub('([a-z0-9])([A-Z])', r'\1_\2', str1).lower()
 
@@ -15,8 +15,8 @@ def stack_inputs(inputs, vars={}, prefix='config_'):
   result = {}
   for key,value in inputs.items():
     try:
-      camel = prefix + camel_case(key)
-      result[key] = vars.get(camel) or value['Default']
+      snake = prefix + snake_case(key)
+      result[key] = vars.get(snake) or value['Default']
     except KeyError as e:
-      raise KeyError("Missing %s variable for %s input.  Please define this variable or specify a 'Default' property for the input." % (camel,key))
+      raise KeyError("Missing %s variable for %s input.  Please define this variable or specify a 'Default' property for the input." % (snake,key))
   return result
