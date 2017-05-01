6853fae60e191cc8933d7e3ef3e9fe615d3a754c
Justin Menga <justin.menga@gmail.com>
2017-05-02 01:12:15 +1200

    Add stack transform logic

--- filter_plugins/stack_transforms.py
+++ filter_plugins/stack_transforms.py
@@ -26,17 +26,16 @@ def lookup_template(file, template_paths=os.getcwd()):
     raise AnsibleError("Could not locate template %s in paths %s" % (file,template_paths))
   return template_file
 
-def render_template(template_file, template_paths, filters={}, **kwargs):
+def render_template(template_file, template_paths, data, filters={}, **kwargs):
   # Create Jinja environment
   environment = Environment()
   environment.loader = FileSystemLoader(template_paths)
   if filters:
     environment.filters = dict(environment.filters.items() + filters.items())
-  # Render template
+  # Render template, passing data in as Config dictionary
   try:
     template = environment.get_template(template_file)
-    template_params = kwargs.get('params') or dict()
-    rendered = template.render(template_params)
+    rendered = template.render({'Config': data})
     return yaml.load(rendered)
   except TemplateNotFound as e:
     raise AnsibleError("Could not locate template %s in the supplied template paths %s" % (template_file,template_paths))
@@ -49,7 +48,36 @@ def ansible_filters(filter_paths):
     filter_loader.add_directory(path)
   return { k:v for filter in filter_loader.all() for k,v in filter.filters().items() }
 
-def resource_transform(data, filter_paths=[],template_paths=[]):
+# Walks stack data structure and searchs and replaces a given parameter
+def search_and_replace(data, search, replace, as_value=False):
+  def parse(key, item, node, parent, parent_key):
+    if key == 'Ref' and item == search:
+      if as_value:
+        parent[parent_key] = replace
+      else:
+        node[key] = replace
+    if key in ['Fn::FindInMap','Fn::GetAtt','Fn::If'] and item[0] == search:
+      node[key][0] = replace
+    if key == 'DependsOn' and search in item:
+      node[key][item.index(search)] = replace
+    if key == 'Fn::Sub' and '${%s' % search in item:
+      if as_value:
+        replace_value = replace
+        if type(replace) is dict and list(set(replace.keys()) & set(['Fn::Sub','Ref'])):
+          replace_value = replace.get('Ref') or replace.get('Fn::Sub')
+        node[key] = item.replace('${%s}' % search, '%s' % replace_value)
+      else:
+        node[key] = item.replace('${%s' % search, '${%s' % replace)
+  def walk(node, parent=[None], parent_key=0):
+    if type(node) is list:
+      for index, item in enumerate(node):
+        walk(item, node, index)
+    elif type(node) is dict:
+      for key, item in node.items():
+        parse(key, item, node, parent, parent_key)
+        walk(item, node, key)
+  walk(data)
+
 def stack_transform(data, filter_paths=[],template_paths=[]):
   filter_paths += [os.getcwd() + '/filter_plugins']
   template_paths += [os.getcwd() + '/templates']
@@ -61,7 +89,7 @@ def stack_transform(data, filter_paths=[],template_paths=[]):
     {
       'name': resource_key,
       'resource': resource_value,
-      'output': render_template(os.path.basename(file), os.path.dirname(file),filters) 
+      'output': render_template(os.path.basename(file), os.path.dirname(file), resource_value, filters) 
     }
     for resource_key, resource_value in data['Resources'].iteritems() 
     if resource_value['Type'] == RESOURCE_TRANSFORM
@@ -69,12 +97,42 @@ def stack_transform(data, filter_paths=[],template_paths=[]):
   ]
   # Merge transforms
   for transform in transforms:
-    creation_policy = transform['resource'].get('CreationPolicy') or "Component"
+    name = transform['name']
+    resource = transform['resource']
+    resource_properties = resource['Properties']
+    output = transform['output']
+    output_parameters = output.get('Parameters')
+    output_parameters = output.get('Parameters')
+    creation_policy = resource.get('CreationPolicy') or "Component"
     if creation_policy == "Child":
       # TODO - handle child stack creation logic
-      continue
+      raise AnsibleError("Child stacks not currently supported")
     else:
-      transform_data = {'Resources': transform['output']['Resources'] }
+      # Post processing of template
+      # Rename keys in Resources, Mappings and Conditions
+      for section in ['Resources','Mappings','Conditions','Outputs']:
+        for key in output.get(section, {}).keys():
+          output[section][name+key] = output[section].pop(key)
+          search_and_replace(output, key, name+key)
+      # Process parameters
+      for output_param_key, output_param_value in output_parameters.items():
+        # Get corresponding transform property
+        resource_property = resource_properties.get(output_param_key)
+        if resource_property:
+          search_and_replace(output, output_param_key, resource_property, as_value=True)
+        else:
+          if output_param_value.get('Default') is None:
+            raise AnsibleError("Transform parameter %s is missing associated transform property and default value" % output_param_key)
+          search_and_replace(output, output_param_key, output_param_value.get('Default'), as_value=True)
+      # Rename main stack references to transform resource
+      for key,value in output.get('Outputs', {}).iteritems():
+        search_and_replace(data,'%s.%s' % (name, key.split(name)[-1]), value['Value'], as_value=True)
+      transform_data = { 
+        'Resources': output['Resources'],
+        'Conditions': output['Conditions'],
+        'Outputs': output['Outputs']
+      }
+
     data = combine(data,transform_data,recursive=True)
     del data['Resources'][transform['name']]
   return data
