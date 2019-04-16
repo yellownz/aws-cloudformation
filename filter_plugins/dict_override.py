1d66ef45c8eade85357923f2f8df9e83584fb9a9
Justin Menga <justin.menga@gmail.com>
2019-04-16 12:21:35 +1200

    Use AWS CLI

--- filter_plugins/dict_override.py
+++ filter_plugins/dict_override.py
@@ -1,9 +1,14 @@
-class FilterModule(object):
-  ''' Returns dictionary of items with keyed overrides that override properties with a matching selector'''
-  def filters(self):
-    return {
-        'dict_override': dict_override
-    }
-
-def dict_override(source, overrides, selector='Type'):
-  return {pk:v for k,v in overrides.items() for pk, pv in source.items() if pv.get(selector) == k}
+class FilterModule(object): 
+  ''' Returns dictionary of items with keyed overrides that override properties with a matching selector''' 
+  def filters(self): 
+    return { 
+        'dict_override': dict_override, 
+        'dict_to_kv_string': dict_to_kv_string 
+    } 
+ 
+def dict_override(source, overrides, selector='Type'): 
+  return {pk:v for k,v in overrides.items() for pk, pv in source.items() if pv.get(selector) == k} 
+ 
+# Returns dict as 'key1=value1<separator>key2=value2 ...' 
+def dict_to_kv_string(source, separator=' '): 
+  return ' '.join([ "{}={}".format(k,v) for k,v in source.items() ])
